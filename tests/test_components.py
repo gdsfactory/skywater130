32e11d454033bab1d3dc794e22776f0bad3842f6
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-04-27 08:28:57 -0700

    adding tests

---
+++ tests/test_components.py
@@ -0,0 +1,31 @@
+import pathlib
+import pytest
+from gdsfactory.component import Component
+from pytest_regressions.data_regression import DataRegressionFixture
+
+from sky130 import cells
+
+
+cell_names = cells.keys()
+dirpath = pathlib.Path(__file__).absolute().with_suffix(".gds")
+
+
+@pytest.fixture(params=cell_names, scope="function")
+def component(request) -> Component:
+    return cells[request.param]()
+
+
+# def test_pdk_gds(component: Component) -> None:
+#     """Avoid regressions in GDS geometry shapes and layers."""
+#     difftest(component, dirpath=dirpath)
+
+
+def test_pdk_settings(
+    component: Component, data_regression: DataRegressionFixture
+) -> None:
+    """Avoid regressions when exporting settings."""
+    data_regression.check(component.to_dict())
+
+
+def test_assert_ports_on_grid(component: Component):
+    component.assert_ports_on_grid()
