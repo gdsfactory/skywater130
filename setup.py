0a888f7005b5b9ba47382bde1b9562e488bc1885
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-21 11:41:11 -0700

    Bump version: 0.0.11 → 0.0.12

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md") as f:
 
 setup(
     name="sky130",
-    version="0.0.11",
+    version="0.0.12",
     url="https://github.com/gdsfactory/skywater130",
     include_package_data=True,
     license="MIT",
