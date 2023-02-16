22048d35215a1891896ff3215dc0cf9745366f97
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-02-16 08:54:52 -0800

    fix line-style

--- sky130/config.py
+++ sky130/config.py
@@ -18,7 +18,7 @@ repo_path = module_path.parent
 class Path:
     module = module_path
     repo = repo_path
-    lyp = module_path / "klayout" / "sky130" / "layers.lyp"
+    lyp = module_path / "klayout" / "layers.lyp"
     libs = module_path / "sky130A"
     sparameters = module_path / "sparameters"
 
