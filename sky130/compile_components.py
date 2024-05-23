a7c2afe5a5a2f5c53c551575f27f2f5037d8a8d0
daquintero <darioaquintero@gmail.com>
2024-05-23 12:54:45 +0200

    :wrench: Components tests should passsing and proper importing

--- sky130/compile_components.py
+++ sky130/compile_components.py
@@ -3,23 +3,32 @@ import os
 # Define the PDK directories
 pdk_directories = ["src/sky130_fd_pr", "src/sky130_fd_sc_hd"]
 
+
 # List to store gds file paths
-gds_files = []
-
-# Walk through the PDK directories and collect GDS files
-for pdk_dir in pdk_directories:
-    for subdir, _, files in os.walk(pdk_dir):
-        for file in files:
-            if file.endswith(".gds"):
-                gds_files.append(os.path.join(subdir, file))
 
 
-# Function to create Python code for each gds file
-def create_code(file_path):
-    file_name = os.path.basename(file_path)
-    cell_name = os.path.splitext(file_name)[0]
+def compile_components(pdk_directories=pdk_directories):
+    gds_files = []
 
-    code = f"""
+    # Walk through the PDK directories and collect GDS files
+    for pdk_dir in pdk_directories:
+        for subdir, _, files in os.walk(pdk_dir):
+            for file in files:
+                if file.endswith(".gds"):
+                    gds_files.append(os.path.join(subdir, file))
+
+    # Function to create Python code for each gds file
+    def create_code(file_path):
+        file_name = os.path.basename(file_path)
+        raw_cell_name = os.path.splitext(file_name)[0]
+
+        for pdk_dir in pdk_directories:
+            prefix = pdk_dir.split("/")[-1] + "__"
+            if raw_cell_name.startswith(prefix):
+                cell_name = raw_cell_name[len(prefix) :]
+                break
+
+        code = f"""
 @cell
 def {cell_name}() -> gf.Component:
     \"\"\"Returns {cell_name} fixed cell.
@@ -32,14 +41,12 @@ def {cell_name}() -> gf.Component:
       c = sky130.components.{cell_name}()
       c.plot()
     \"\"\"
-    return import_gds("{file_path}", cellname="{cell_name}")
+    return import_gds("{file_path}", cellname="{raw_cell_name}")
 """
-    return code
+        return code
 
-
-# Prelude to add at the top of the file
-prelude = """from functools import partial
-
+    # Prelude to add at the top of the file
+    prelude = """from functools import partial
 import gdsfactory as gf
 from gdsfactory.cell import cell
 
@@ -83,18 +90,21 @@ gdsdir = PATH.module
 import_gds = partial(gf.import_gds, gdsdir=gdsdir, decorator=add_ports)
 """
 
-# Write the code to a Python file
-with open("components.py", "w") as f:
-    f.write(prelude)
-    for gds_file in gds_files:
-        gdsdir = os.path.dirname(gds_file)
-        code = create_code(gds_file)
-        f.write(code)
+    # TODO delete old file automatically
+    # Write the code to a Python file
+    with open("components.py", "w") as f:
+        f.write(prelude)
+        for gds_file in gds_files:
+            print(gds_file)
+            # gds_file = os.path.dirname(str(pathlib.Path(gds_file)))
+            code = create_code(gds_file)
+            f.write(code)
 
-print("Python file 'components.py' has been created.")
+    print("Python file 'components.py' has been created.")
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    compile_components()
 #     # gf.write_cells.write_cells(gdspath=PATH.gdshd, dirpath="gds")
 #     # gf.write_cells.write_cells(gdspath=PATH.gdshs, dirpath="gds")
 #     # gf.write_cells.write_cells(gdspath=PATH.gdshvl, dirpath="gds")
